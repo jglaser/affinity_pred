fix more add/multiply issues with the attention mask

affinity_pred/ensemble_embedding.py
@@ -17,6 +17,7 @@ class BertHAttention1D(nn.Module):
         self,
         config,
         local_block_size=16, # for tokens within this distance we always use the full attention
+        mask_mode='add',
     ):
         super().__init__()
 
@@ -36,6 +37,11 @@ class BertHAttention1D(nn.Module):
 
         self.attn.to_qkv = torch.nn.Identity() # passthrough
         self.attn.to_out = torch.nn.Identity()
+
+        if mask_mode != 'add' and mask_mode != 'mul':
+            raise ValueError
+
+        self.mask_mode = mask_mode
 
     def forward(
         self,
@@ -62,14 +68,24 @@ class BertHAttention1D(nn.Module):
         qkv = torch.flatten(qkv, start_dim=2)
 
         if attention_mask is not None:
+            if self.mask_mode == 'add':
+                # make boolean (multiplicative)
+                attention_mask = attention_mask >= 0
+            for dim in range(attention_mask.dim()-1,0,-1):
+                attention_mask = attention_mask.squeeze(dim)
             attention_mask = attention_mask.type(torch.bool)
 
         if encoder_attention_mask is not None:
+            if self.mask_mode == 'add':
+                # make boolean (multiplicative)
+                encoder_attention_mask = encoder_attention_mask >= 0
+            for dim in range(encoder_attention_mask.dim()-1,0,-1):
+                encoder_attention_mask = encoder_attention_mask.squeeze(dim)
             encoder_attention_mask = encoder_attention_mask.type(torch.bool)
 
         context_layer = self.attn(qkv,
             mask=attention_mask,
-            key_value_mask = encoder_attention_mask
+            key_value_mask=encoder_attention_mask
         )
 
         outputs = (context_layer, )
@@ -80,7 +96,8 @@ class CrossAttentionLayer(nn.Module):
     def __init__(self,
             config,
             other_config,
-            use_hierarchical_attention=False):
+            use_hierarchical_attention=False,
+            mask_mode='mul'):
         super().__init__()
         self.chunk_size_feed_forward = config.chunk_size_feed_forward
         self.seq_len_dim = 1
@@ -88,7 +105,12 @@ class CrossAttentionLayer(nn.Module):
         self.crossattention = BertAttention(config)
 
         if use_hierarchical_attention:
-            self.crossattention.self = BertHAttention1D(config)
+            self.crossattention.self = BertHAttention1D(
+                config=config,
+                mask_mode=mask_mode)
+        else:
+            if mask_mode != 'add':
+                raise NotImplementedError
 
         self.intermediate = BertIntermediate(config)
         self.output = BertOutput(config)
@@ -153,10 +175,25 @@ class EnsembleEmbedding(torch.nn.Module):
         self.config = BertConfig(hidden_size = self.seq_model.config.hidden_size + self.smiles_model.config.hidden_size)
 
         # upgrade the self-attention layers to hierarchical ones
-        layers = self.seq_model.encoder.layer
+        seq_layers = self.seq_model.encoder.layer
 
-        for layer in layers:
-            h_attention = BertHAttention1D(config=seq_config)
+        for layer in seq_layers:
+            h_attention = BertHAttention1D(
+                config=seq_config,
+                mask_mode='add'
+            )
+            h_attention.query = layer.attention.self.query
+            h_attention.key = layer.attention.self.key
+            h_attention.value = layer.attention.self.value
+
+            layer.attention.self = h_attention
+
+        smiles_layers = self.smiles_model.encoder.layer
+        for layer in smiles_layers:
+            h_attention = BertHAttention1D(
+                config=smiles_config,
+                mask_mode='add'
+            )
             h_attention.query = layer.attention.self.query
             h_attention.key = layer.attention.self.key
             h_attention.value = layer.attention.self.value
@@ -181,14 +218,14 @@ class EnsembleEmbedding(torch.nn.Module):
                     use_hierarchical_attention=True) for _ in range(n_cross_attention_layers)])
                 self.cross_attention_smiles = nn.ModuleList([CrossAttentionLayer(config=smiles_config,
                     other_config=seq_config,
-                    use_hierarchical_attention=False) for _ in range(n_cross_attention_layers)])
+                    use_hierarchical_attention=True) for _ in range(n_cross_attention_layers)])
         else:
             self.cross_attention_seq = nn.ModuleList([CrossAttentionLayer(config=seq_config,
                 other_config=smiles_config,
                 use_hierarchical_attention=True) for _ in range(n_cross_attention_layers)])
             self.cross_attention_smiles = nn.ModuleList([CrossAttentionLayer(config=smiles_config,
                 other_config=seq_config,
-                use_hierarchical_attention=False) for _ in range(n_cross_attention_layers)])
+                use_hierarchical_attention=True) for _ in range(n_cross_attention_layers)])
 
     def forward(
             self,
@@ -203,7 +240,6 @@ class EnsembleEmbedding(torch.nn.Module):
         outputs = []
         input_ids_1 = input_ids[:,:self.max_seq_length]
         attention_mask_1 = attention_mask[:,:self.max_seq_length]
-        inv_attention_mask_1 = self.seq_model.invert_attention_mask(attention_mask_1)
 
         input_shape = input_ids_1.size()
         device = input_ids_1.device
@@ -223,7 +259,6 @@ class EnsembleEmbedding(torch.nn.Module):
         input_ids_2 = input_ids[:,self.max_seq_length:]
         input_shape = input_ids_2.size()
         attention_mask_2 = attention_mask[:,self.max_seq_length:]
-        inv_attention_mask_2 = self.smiles_model.invert_attention_mask(attention_mask_2)
 
         encoder_outputs = self.smiles_model(input_ids=input_ids_2,
                                          attention_mask=attention_mask_2,
@@ -264,9 +299,9 @@ class EnsembleEmbedding(torch.nn.Module):
             position_ids=position_ids,
         )
 
-        for i in range(self.n_cross_attention_layers):
-            padded_smiles = torch.cat([hidden_smiles, pad_inputs_embeds], dim=-2)
+        padded_smiles = torch.cat([hidden_smiles, pad_inputs_embeds], dim=-2)
 
+        for i in range(self.n_cross_attention_layers):
             attention_output_1 = self.cross_attention_seq[i](
                 hidden_states=hidden_seq,
                 attention_mask=attention_mask_1,
@@ -275,14 +310,16 @@ class EnsembleEmbedding(torch.nn.Module):
                 output_attentions=output_attentions)
 
             attention_output_2 = self.cross_attention_smiles[i](
-                hidden_states=hidden_smiles,
-                attention_mask=inv_attention_mask_2,
+                hidden_states=padded_smiles,
+                attention_mask=padded_attention_mask_2,
                 encoder_hidden_states=hidden_seq,
-                encoder_attention_mask=inv_attention_mask_1,
+                encoder_attention_mask=attention_mask_1,
                 output_attentions=output_attentions)
 
             hidden_seq = attention_output_1[0]
-            hidden_smiles = attention_output_2[0]
+            padded_smiles = attention_output_2[0]
+
+        hidden_smiles = padded_smiles[:,:hidden_smiles.size()[1]]
 
         mean_seq = torch.mean(hidden_seq,axis=1)
         mean_smiles = torch.mean(hidden_smiles,axis=1)
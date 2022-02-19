c53251c2e76499c3d15044aadd93ea90d16d4ee9
Jens Glaser <glaserj@ornl.gov>
2022-02-19 17:53:10 -0500

    return numpy outputs

--- affinity_pred/data_utils.py
+++ affinity_pred/data_utils.py
@@ -1,5 +1,15 @@
 from transformers import DataCollatorWithPadding, default_data_collator, PreTrainedTokenizerBase
+
+from transformers import Pipeline
+from transformers.pipelines.pt_utils import PipelineDataset, PipelineIterator
+
+from torch.utils.data import DataLoader
+
 from typing import List, Dict, Any
+
+import os
+import collections
+
 
 class EnsembleDataCollatorWithPadding:
     def __init__(self,
@@ -35,14 +45,81 @@ class EnsembleTokenizer:
         self.smiles_tokenizer = smiles_tokenizer
         self.seq_tokenizer = seq_tokenizer
 
-    def __call__(self, features):
+    def __call__(self, features, **kwargs):
         item = {}
-        seq_encodings = self.seq_tokenizer(features['seq'])
+
+        is_batched = isinstance(features, (list, tuple))
+
+        if is_batched:
+            seq_encodings = self.seq_tokenizer([f['seq'] for f in features])
+        else:
+            seq_encodings = self.seq_tokenizer(features['seq'], **kwargs)
+
         item['input_ids_1'] = seq_encodings['input_ids']
         item['attention_mask_1'] = seq_encodings['attention_mask']
 
-        smiles_encodings = self.smiles_tokenizer(features['smiles_canonical'])
+        if is_batched:
+            smiles_encodings = self.smiles_tokenizer([f['smiles_canonical'] for f in features], **kwargs)
+        else:
+            smiles_encodings = self.smiles_tokenizer(features['smiles_canonical'], **kwargs)
+
         item['input_ids_2'] = smiles_encodings['input_ids']
         item['attention_mask_2'] = smiles_encodings['attention_mask']
 
         return item
+
+class ProteinLigandScoring(Pipeline):
+    def _sanitize_parameters(self, **kwargs):
+        preprocess_kwargs = {}
+        return preprocess_kwargs, {}, {}
+
+    def __init__(self,
+        model,
+        seq_tokenizer,
+        smiles_tokenizer,
+        **kwargs
+        ):
+        self.seq_tokenizer = seq_tokenizer
+        self.smiles_tokenizer = smiles_tokenizer
+        self.data_collator = EnsembleDataCollatorWithPadding(self.smiles_tokenizer,
+                                                             self.seq_tokenizer)
+        super().__init__(model=model,
+                         tokenizer=EnsembleTokenizer(self.smiles_tokenizer,
+                                                    self.seq_tokenizer),
+                         **kwargs)
+        
+    def preprocess(self, inputs):
+        tokenized_input = self.tokenizer(inputs)#, return_tensors=self.framework) 
+        return tokenized_input
+
+    def _forward(self, model_inputs):
+        outputs = self.model(**model_inputs)
+        return outputs
+
+    def postprocess(self, model_outputs):
+        return model_outputs.numpy()
+    
+
+    def get_iterator(
+        self, inputs, num_workers: int, batch_size: int, preprocess_params, forward_params, postprocess_params
+    ):
+        if isinstance(inputs, collections.abc.Sized):
+            dataset = PipelineDataset(inputs, self.preprocess, preprocess_params)
+        else:
+            if num_workers > 1:
+                logger.warning(
+                    "For iterable dataset using num_workers>1 is likely to result"
+                    " in errors since everything is iterable, setting `num_workers=1`"
+                    " to guarantee correctness."
+                )
+                num_workers = 1
+            dataset = PipelineIterator(inputs, self.preprocess, preprocess_params)
+        if "TOKENIZERS_PARALLELISM" not in os.environ:
+            logger.info("Disabling tokenizer parallelism, we're using DataLoader multithreading already")
+            os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+        collate_fn = self.data_collator
+        dataloader = DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, collate_fn=collate_fn)
+        model_iterator = PipelineIterator(dataloader, self.forward, forward_params, loader_batch_size=batch_size)
+        final_iterator = PipelineIterator(model_iterator, self.postprocess, postprocess_params)
+        return final_iterator
